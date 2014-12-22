248e49b4e7fc2abd1a3dcf9c84f4b87601149919
Jesse Temple <jesse@stonefin.com>
2014-12-22 09:20:02 -0600

    new project with reproduced horizontal scrolling bug

---
+++ SyncfusionProof/SyncfusionProof/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using SyncfusionProof.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SyncfusionProof.Controllers
+{
+  public class HomeController : Controller
+  {
+    public ActionResult Index()
+    {
+      TestModel stuff = new TestModel()
+      {
+        OrderId = 2031532465,
+        CustomerId = "5423115657865424ASFE",
+        EmployeeId = int.MaxValue,
+        Freight = "$32.50",
+        ShipCity = "Chicago",
+        ShipName = "USS Enterprise",
+        OrderDate = DateTime.Now,
+        ShipState = "Illinois",
+        FavoriteBunny = "Reader Rabbit"
+      };
+      var StuffList = new List<TestModel>();
+      StuffList.Add(stuff);
+      stuff.OrderId = 124;
+      StuffList.Add(stuff);
+
+      return View("Index", StuffList);
+    }
+
+    public ActionResult About()
+    {
+      ViewBag.Message = "Your application description page.";
+
+      return View();
+    }
+
+    public ActionResult Contact()
+    {
+      ViewBag.Message = "Your contact page.";
+
+      return View();
+    }
+  }
+}
---
+++ SyncfusionProof/SyncfusionProof/Models/TestModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SyncfusionProof.Models
+{
+  public class TestModel
+  {
+    public int OrderId { get; set; }
+    public string CustomerId { get; set; }
+    public int EmployeeId { get; set; }
+    public string Freight { get; set; }
+    public string ShipCity { get; set; }
+    public string ShipName { get; set; }
+    public string ShipState { get; set; }
+    public DateTime OrderDate { get; set; }
+    public string FavoriteBunny { get; set; }
+  }
+}
