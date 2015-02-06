2c815d37ac1fe6d83ab405f127c6d856154d164c
Jesse Temple <jesse@stonefin.com>
2015-02-06 14:44:42 -0600

    updated syncfusion package, played around with resizing/scrolling/responsive grid settings for the latest syncfusion incident report

--- SyncfusionProof/SyncfusionProof/Scripts/Utility/Syncfusion.js
+++ SyncfusionProof/SyncfusionProof/Scripts/Utility/Syncfusion.js
@@ -1,23 +1,23 @@
 ﻿$(function () {
-  //tab declaration
-  $("#TestTabs").ejTab();
+  ////tab declaration
+  //$("#TestTabs").ejTab();
 
-  var gridObj = $("#Grid").ejGrid("instance");
+  //var gridObj = $("#Grid").ejGrid("instance");
 
-  scrolWidth = gridObj.model.scrollSettings.width / $(".cols-sample-area").width();
+  //scrolWidth = gridObj.model.scrollSettings.width / $(".cols-sample-area").width();
 
-  if (gridObj.element.width() > $(".cols-sample-area").width()) {
-    var scrollerwidth = Math.floor($(".cols-sample-area").width())
+  //if (gridObj.element.width() > $(".cols-sample-area").width()) {
+  //  var scrollerwidth = Math.floor($(".cols-sample-area").width())
 
-    gridObj.option("model.scrollSettings", { width: scrollerwidth })
+  //  gridObj.option("model.scrollSettings", { width: scrollerwidth })
 
-    scrolWidth = 1;
-  }
+  //  scrolWidth = 1;
+  //}
 });
 
-$(window).resize(function () {
-  var gridObj = $("#Grid").ejGrid("instance")
-  var scrollerwidth = Math.floor($(".cols-sample-area").width() * scrolWidth)
+//$(window).resize(function () {
+//  var gridObj = $("#Grid").ejGrid("instance")
+//  var scrollerwidth = Math.floor($(".cols-sample-area").width() * scrolWidth)
 
-  gridObj.option("model.scrollSettings", { width: scrollerwidth })
-});
+//  gridObj.option("model.scrollSettings", { width: scrollerwidth })
+//});
