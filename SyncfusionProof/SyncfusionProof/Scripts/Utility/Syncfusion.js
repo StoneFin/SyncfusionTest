3a2ada5f4dc28795fe59331b52a4974dd88d09db
Jesse Temple <jesse@stonefin.com>
2015-02-09 11:59:34 -0600

    troubleshooting scrolling, width, responsive, etc

--- SyncfusionProof/SyncfusionProof/Scripts/Utility/Syncfusion.js
+++ SyncfusionProof/SyncfusionProof/Scripts/Utility/Syncfusion.js
@@ -1,23 +1,3 @@
 ﻿$(function () {
-  ////tab declaration
-  //$("#TestTabs").ejTab();
-
-  //var gridObj = $("#Grid").ejGrid("instance");
-
-  //scrolWidth = gridObj.model.scrollSettings.width / $(".cols-sample-area").width();
-
-  //if (gridObj.element.width() > $(".cols-sample-area").width()) {
-  //  var scrollerwidth = Math.floor($(".cols-sample-area").width())
-
-  //  gridObj.option("model.scrollSettings", { width: scrollerwidth })
-
-  //  scrolWidth = 1;
-  //}
+  
 });
-
-//$(window).resize(function () {
-//  var gridObj = $("#Grid").ejGrid("instance")
-//  var scrollerwidth = Math.floor($(".cols-sample-area").width() * scrolWidth)
-
-//  gridObj.option("model.scrollSettings", { width: scrollerwidth })
-//});
