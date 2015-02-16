ff7ef1b37851554941b0073baefa5218aa2bf8ac
Jesse Temple <jesse@stonefin.com>
2015-02-16 11:24:30 -0600

    trying Syncfusion's scrolling grid tab fix (didn't work)

--- SyncfusionProof/SyncfusionProof/Scripts/Utility/Syncfusion.js
+++ SyncfusionProof/SyncfusionProof/Scripts/Utility/Syncfusion.js
@@ -1,3 +1,23 @@
 ﻿$(function () {
-  
+  //tabbed grid test
+  var onItemActive = function (args) {
+    console.log("onItemActive");
+
+    //skip the action for the first tab item
+    if (args.activeIndex == args.prevActiveIndex) {
+      return;
+    }
+
+    //get the current tab
+    var activeGrid = $(args.activeHeader).find("a").attr("href");
+
+    //find the grid inside the current tab and apply scrolling
+    $(activeGrid).find(".e-grid").ejGrid({
+      allowScrolling: true,
+      scrollSettings: { width: "100%" }
+    });
+
+    //refresh the grid content
+    $(activeGrid).find(".e-grid").ejGrid("refreshContent");
+  }
 });
