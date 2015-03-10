949d807eec4c33c9245f5d2d5a69b251da0e4b04
Jesse Temple <jesse@stonefin.com>
2015-03-10 11:00:14 -0500

    script and css bundle cleanup

--- SyncfusionProof/SyncfusionProof/App_Start/BundleConfig.cs
+++ SyncfusionProof/SyncfusionProof/App_Start/BundleConfig.cs
@@ -7,33 +7,31 @@ namespace SyncfusionProof
   {
     // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
 
-    private static readonly string[] Syncfusion = 
-    {
-      "~/Scripts/Utility/Syncfusion*"
-    };
-    
     public static void RegisterBundles(BundleCollection bundles)
     {
-      bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                  "~/Scripts/jquery-{version}.js"));
+      bundles.Add(new StyleBundle("~/content/css")
+        .Include("~/Content/bootstrap.css")
+        .Include("~/Content/bootstrap-theme.css")
+        .Include("~/Content/site.css")
+        .Include("~/Content/ej/web/ej.widgets.core.min.css")
+        .Include("~/Content/ej/web/default-theme/ej.web.all.min.css")
+        );
 
-      bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                  "~/Scripts/jquery.validate*"));
-
-      // Use the development version of Modernizr to develop with and learn from. Then, when you're
-      // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-      bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                  "~/Scripts/modernizr-*"));
-
-      bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                "~/Scripts/bootstrap.js",
-                "~/Scripts/respond.js"));
-
-      bundles.Add(new StyleBundle("~/Content/css").Include(
-                "~/Content/bootstrap.css",
-                "~/Content/site.css"));
-
-      bundles.Add(new ScriptBundle("~/Bundles/Syncfusion").Include(Syncfusion));
+      //Use the development version of Modernizr to develop with and learn from. Then, when you're
+      //ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+      bundles.Add(new ScriptBundle("~/bundles/layout")
+        .Include("~/Scripts/modernizr-*")
+        .Include("~/Scripts/jquery-{version}.js")
+        .Include("~/Scripts/jquery.validate*")
+        .Include("~/Scripts/jquery.easing.{version}.js")
+        .Include("~/Scripts/jquery.globalize.min.js")
+        .Include("~/Scripts/jsrender*")
+        .Include("~/Scripts/ej/ej.web.all.min.js")
+        .Include("~/Scripts/ej/ej.unobtrusive.min.js")
+        .Include("~/Scripts/bootstrap*")
+        .Include("~/Scripts/respond*")
+        .Include("~/Scripts/Utility/Syncfusion*")
+        );
 
       // Set EnableOptimizations to false for debugging. For more information,
       // visit http://go.microsoft.com/fwlink/?LinkId=301862
