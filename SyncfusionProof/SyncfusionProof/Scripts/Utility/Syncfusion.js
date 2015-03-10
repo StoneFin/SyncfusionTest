cbf64d384821b96111738a3734fab0d236d01599
Jesse Temple <jesse@stonefin.com>
2015-03-10 09:27:53 -0500

    grid layout updates

--- SyncfusionProof/SyncfusionProof/Scripts/Utility/Syncfusion.js
+++ SyncfusionProof/SyncfusionProof/Scripts/Utility/Syncfusion.js
@@ -2,20 +2,7 @@
  
 });
 
-//tabbed grid test, cannot be contained in document ready, per Syncfusion
-//http://www.syncfusion.com/support/directtrac/incidents/133501
-/*
- * We have analyzed your code snippet and found that you have called the 
- * event function inside the JQuery document ready, which is the cause of 
- * the issue. We would like to let you know that when we defined the event 
- * function inside the document ready, the control cant able to access on 
- * that function because it’s not rendered at that time. So we suggest you 
- * to call the event function outside of document ready to resolve the 
- * issue. Please refer the below code snippets for further details.
- */
 var onItemActive = function (args) {
-  //console.log("onItemActive");
-
   //skip the action for the first tab item
   if (args.activeIndex == args.prevActiveIndex) {
     return;
