ac71369db982e08bb590bb7bf92f9b56abb175eb
Jesse Temple <jesse@stonefin.com>
2015-05-06 09:19:43 -0500

    moved cancel flag setting to the top of the toolbarClick function

--- SyncfusionProof/SyncfusionProof/Scripts/Utility/Syncfusion.js
+++ SyncfusionProof/SyncfusionProof/Scripts/Utility/Syncfusion.js
@@ -68,11 +68,11 @@ var deleteRecords = function (gridId, gridKey, records) {
 var toolbarClick = function (toolbarItem) {
   var self = this;
 
+  //cancel the grid's default operation
+  toolbarItem.cancel = true;
+
   if (toolbarItem.itemName === "Delete") {
     var records = getRecords(toolbarItem, self);
-
-    //cancel the grid's default delete operation
-    toolbarItem.cancel = true;
 
     deleteRecords("MultiSelectGroupedGrid", "OrderId", records);
   }
