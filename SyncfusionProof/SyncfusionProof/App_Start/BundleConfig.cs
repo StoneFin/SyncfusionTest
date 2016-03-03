1d5ced22eeff5d79354f8ee28727a757272b2df7
Jesse Temple <jesse@stonefin.com>
2016-03-03 11:15:29 -0600

    cleaned up bundleconfig; tried Syncfusion bootstrap theme and didn't like it

--- SyncfusionProof/SyncfusionProof/App_Start/BundleConfig.cs
+++ SyncfusionProof/SyncfusionProof/App_Start/BundleConfig.cs
@@ -20,16 +20,19 @@ namespace SyncfusionProof
         .Include("~/Content/bootstrap-theme.css")
         .Include("~/Content/site.css")
         );
+
+      //we must include Syncfusion css in a separate bundle for optimizations to work properly
       
-      //we must include ej.widgets.all.min.css in a separate bundle for optimizations to work properly
+      //original
       //bundles.Add(new StyleBundle("~/content/ejwidgetsall")
       //  .Include("~/Content/ej/web/default-theme/ej.widgets.all.min.css", new CssRewriteUrlTransform())
       //  );
 
-      //generated a single custom file, now there's no visible grids or uploadbox
+      //custom generated css
+      //http://csg.syncfusion.com/combine
       bundles.Add(new StyleBundle("~/content/ejwidgetsall")
-         .Include("~/Content/SyncfusionCustom/default-theme/ej.web.all.css", new CssRewriteUrlTransform())
-         );
+        .Include("~/Content/SyncfusionCustom/default-theme/ej.web.all.css", new CssRewriteUrlTransform())
+        );
 
       //Use the development version of Modernizr to develop with and learn from. Then, when you're
       //ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
@@ -45,7 +48,8 @@ namespace SyncfusionProof
         //.Include("~/Scripts/ej/ej.web.all.min.js")
         //.Include("~/Scripts/ej/ej.unobtrusive.min.js")
 
-        //generated a single custom file, now there's no visible grids or uploadbox
+        //custom generated js
+        //http://csg.syncfusion.com/combine
         .Include("~/Scripts/SyncfusionCustom/ej.gridupload.all.js")
         .Include("~/Scripts/ej/ej.unobtrusive.min.js")
 
