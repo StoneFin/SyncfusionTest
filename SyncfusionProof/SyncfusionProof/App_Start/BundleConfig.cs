713a85d41ed3f4029bdf871068c2e16bd5d18e67
Jesse Temple <jesse@stonefin.com>
2016-08-31 10:39:52 -0500

    updated nuget packages

--- SyncfusionProof/SyncfusionProof/App_Start/BundleConfig.cs
+++ SyncfusionProof/SyncfusionProof/App_Start/BundleConfig.cs
@@ -256,10 +256,51 @@ namespace SyncfusionProof
     //}
     #endregion
 
-    #region Syncfusion 14.2.0.26, custom generated css/script
+    #region Syncfusion 14.2.0.28
+    //public static void RegisterBundles(BundleCollection bundles)
+    //{
+    //  //manually enable minification
+    //  //running in debug sets this to false and release sets it to true
+    //  //you don't have to manually set this unless you're troubleshooting something
+    //  //or if you want to publish minified code in debug mode for dev deployments
+    //  //BundleTable.EnableOptimizations = true;
+
+    //  bundles.Add(new StyleBundle("~/content/css")
+    //    .Include("~/Content/bootstrap.css")
+    //    .Include("~/Content/bootstrap-theme.css")
+    //    .Include("~/Content/site.css")
+    //    );
+
+    //  //we must include Syncfusion css in a separate bundle for optimizations to work properly
+    //  bundles.Add(new StyleBundle("~/content/ejwidgetsall")
+    //    .Include("~/Content/ej/web/default-theme/ej.widgets.all.min.css", new CssRewriteUrlTransform())
+    //    );
+
+    //  //Use the development version of Modernizr to develop with and learn from. Then, when you're
+    //  //ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+    //  bundles.Add(new ScriptBundle("~/bundles/layout")
+    //    .Include("~/Scripts/modernizr-*")
+    //    .Include("~/Scripts/jquery-{version}.js")
+    //    .Include("~/Scripts/jquery.validate*")
+    //    .Include("~/Scripts/jquery.easing.{version}.js")
+    //    .Include("~/Scripts/jquery.globalize.min.js")
+    //    .Include("~/Scripts/jsrender.min.js")
+    //    .Include("~/Scripts/ej/web/ej.web.all.min.js")
+    //    .Include("~/Scripts/ej/common/ej.unobtrusive.min.js")
+    //    .Include("~/Scripts/bootstrap*")
+    //    .Include("~/Scripts/amplify*")
+    //    .Include("~/Scripts/respond*")
+    //    .Include("~/Scripts/underscore*")
+    //    .Include("~/Scripts/Utility/Syncfusion*")
+    //    );
+
+    //  bundles.Add(new ScriptBundle("~/bundles/report/withparameters")
+    //    .Include("~/Scripts/Utility/ReportViewer*")
+    //    );
+    //}
     #endregion
 
-    #region Syncfusion 14.2.0.28
+    #region Syncfusion 14.2.0.32
     public static void RegisterBundles(BundleCollection bundles)
     {
       //manually enable minification
